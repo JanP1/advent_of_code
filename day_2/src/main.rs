2571089e9b5b6ebc71422a244fce1f9b73fae944
Jan <jan@fedora.local>
2025-12-03 23:01:21 +0100

    Char managing function - wrong approach

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,21 +1,9 @@
+#![allow(warnings)]
+
 use std::io::Read;
 use utf8_read::Reader;
 
 
-/*
- * Action enum
- * Represents either the ending of the first number - lower bound
- * or the second number - upper bound
- * or just a number char
- * or an error
-*/
-enum Action {
-    AddLowerBounds(i32),
-    AddUpperBounds(i32),
-    ReadNumChar(i32),
-    Error,
-
-}
 
 fn main() {
     let file = std::fs::File::open("input.txt").unwrap();
@@ -26,16 +14,21 @@ fn main() {
 }
 
 
-fn sum_of_invalid_ids<R>(reader: &mut Reader<R>) -> i32 where R: Read {
+fn sum_of_invalid_ids<R>(reader: &mut Reader<R>) -> u32 where R: Read {
     
-    let mut sum = 0;
+    let mut sum: u32 = 0;
+    let mut lower_bound: String = "".to_string();
+    let mut upper_bound: String = "".to_string();
+    let mut lower_b_comleted: bool = false;
 
 
     for c in reader {
         match c {
-            Ok(x) => {
+            Ok(ch) => {
                 // checking the current char
-                print!("{x} ... "); 
+                handle_current_char(ch, sum, &mut lower_bound, &mut lower_b_comleted);
+                println!("{lower_bound} ... "); 
+                
 
 
             }
@@ -46,12 +39,24 @@ fn sum_of_invalid_ids<R>(reader: &mut Reader<R>) -> i32 where R: Read {
     0
 }
 
-fn handle_current_char(c: char) -> Action {
-    match  c {
-        ' ' => {}
-        _ => {}
-        
-    }
 
-    Action::ReadNumChar(5)
+fn handle_current_char(c: char, sum: u32, number: &mut String, l_b_completed: &mut bool) {
+    match  c {
+        '-' => {
+            *l_b_completed = true;
+        }
+        ',' => {
+            *l_b_completed = false;
+        }
+        _ => {
+            if 
+            number.push(c);
+        }
+    }
 }
+
+
+fn sum_wrong_ids_in_range(lower_b: u32, upper_b: u32) {
+
+}
+
